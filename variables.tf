f5dd2b363961637851c629e6677acbf655fea81b
abdulsalamkp2003 <abdulsalamkp2003@gmail.com>
2026-07-29 01:46:12 +0800

    rootVariableFixes

--- variables.tf
+++ variables.tf
@@ -15,3 +15,20 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+
+variable "name" {
+  type    = string
+  default = "my-practice-ec2"
+}
+
+variable "ami" {
+  type        = string
+  description = "AMI ID for the EC2 instance"
+  default = ""
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
